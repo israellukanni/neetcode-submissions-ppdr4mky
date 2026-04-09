27059af9cfc27337893cb7e15d2cf2a116b28bd7
Israel Olukanni <123609190+israellukanni@users.noreply.github.com>
2026-04-09 17:21:34 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hashy = defaultdict(int)
+
+        for i,n in enumerate(nums):
+            if target - n in hashy:
+                return [hashy[target-n],i]
+            hashy[n] = i
+        
