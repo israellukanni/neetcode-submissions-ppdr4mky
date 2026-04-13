d6427d311d6394f210d3481c087ed22bd81f432a
Israel Olukanni <123609190+israellukanni@users.noreply.github.com>
2026-04-13 16:02:35 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        hashS,hashT = defaultdict(int), defaultdict(int)
+
+        for l in s:
+            hashS[l] += 1
+        for l in t:
+            hashT[l] += 1
+        return hashS == hashT
