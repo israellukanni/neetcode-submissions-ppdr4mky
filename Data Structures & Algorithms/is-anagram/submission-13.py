1ea25be7bae1857285d068030ba2cf8f294ff7f5
Israel Olukanni <123609190+israellukanni@users.noreply.github.com>
2026-05-08 00:01:10 -0400

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        
+        hs, ht = defaultdict(int), defaultdict(int)
+
+        for i in range(len(s)):
+            hs[s[i]] += 1
+            ht[t[i]] += 1
+        return hs == ht
