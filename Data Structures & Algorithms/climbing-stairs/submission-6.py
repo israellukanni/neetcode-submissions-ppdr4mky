106a2d3f6ffa1025e0cefe5802e156940580723e
Israel Olukanni <123609190+israellukanni@users.noreply.github.com>
2026-05-19 16:25:05 -0400

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        memo = defaultdict(int)
+        memo[1] = 1
+        memo[2] = 2
+        if n < 3:
+            return memo[n]
+        for i in range(3,n):
+            memo[i] = memo[i-1] + memo[i-2]
+        return memo[n-1] + memo[n-2]
