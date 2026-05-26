277da267d09eeb8bda7592828254d712bf14d0ba
Israel Olukanni <123609190+israellukanni@users.noreply.github.com>
2026-05-26 15:15:55 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = list()
+        # self.minStack = list()
+
+    def push(self, val: int) -> None:
+        # self.stack.append(val)
+        minval = min(val, self.stack[-1][1] if self.stack else val)
+        self.stack.append((val,minval))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
